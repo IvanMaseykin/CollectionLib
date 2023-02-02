fe6ac6e21cde82d62123e247f8d09799a5e19db9
user <user@K218PC9>
2023-02-02 12:27:31 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace CollectionLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var people = new ObservableCollection<string>
+            {
+                "Tom", "Bob", "Sam"
+            };
+
+            foreach (var person in people)
+            {
+                Console.WriteLine(person);
+            }
+            for (int i = 0; i < people.Count; i++)
+            {
+                Console.WriteLine(people[i]);
+            }
+        }
+    }
+}
